ce85bfbbf6d78cb5606bb960cb85815581d3766b
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2021-01-13 20:44:20 +0530

    test cases added

--- src/test/java/com/cims/jasper/ReportsTest.java
+++ src/test/java/com/cims/jasper/ReportsTest.java
@@ -5,20 +5,17 @@ import com.cims.jasper.controllers.vm.ReportDTO;
 import com.cims.jasper.model.StockItem;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ReportsController.class)
@@ -34,25 +31,24 @@ public class ReportsTest {
     public void testWithSampleData() throws Exception {
 
         StockItem item = new StockItem();
-            item.setItemName("Name");
-            item.setItemCapacity(500.F);
-            item.setMeasUnit("g");
-            item.setTotalQuantity(5.0F);
-            item.setStockBookFolio("110-11");
-            item.setUnitPrice(1000.00F);
+        item.setItemName("Name");
+        item.setItemCapacity(500.F);
+        item.setMeasUnit("g");
+        item.setTotalQuantity(5.0F);
+        item.setStockBookFolio("110-11");
+        item.setUnitPrice(1000.00F);
 
         List<StockItem> list = new ArrayList<>();
         list.add(item);
 
         ReportDTO reportDTO = new ReportDTO();
-        reportDTO.setName("StockItem");
-        reportDTO.setTitle("Title");
-        reportDTO.setPayload(Collections.singletonList(list));
+        reportDTO.setPayload(Arrays.asList(list.toArray()));
+        reportDTO.setTitle("TEST");
 
 
-        this.mockMvc.perform(post("/report")
+        this.mockMvc.perform(post("/report?name=StockItem")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsBytes(reportDTO)))
+                .content(mapper.writeValueAsString(reportDTO)))
                 .andExpect(status().isOk());
     }
 }
